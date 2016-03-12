6959adcd7b2a9c3853b1c015973ed0ec7cf5cc8f
mcNETDev <danielwieckhorst1337@web.de>
2016-03-12 12:53:39 +0100

    added Scanner

--- DiscordTest/src/main/java/eu/hostcode/discordTest/Main.java
+++ DiscordTest/src/main/java/eu/hostcode/discordTest/Main.java
@@ -1,7 +1,11 @@
 package eu.hostcode.discordTest;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hey");
+		Scanner s = new Scanner(System.in);
+		System.out.println(s.nextLine());
 	}
 }
